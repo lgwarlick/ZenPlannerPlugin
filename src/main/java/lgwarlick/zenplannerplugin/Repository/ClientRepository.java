308fee4a0ce83ddefdaaf6c91c20a70c85ec9450
Logan <lgwarlick@gmail.com>
2021-05-31 11:43:40 -0500

    replaced CrudRepository with JpaRepository

--- src/main/java/lgwarlick/zenplannerplugin/Repository/ClientRepository.java
+++ src/main/java/lgwarlick/zenplannerplugin/Repository/ClientRepository.java
@@ -1,7 +1,8 @@
 package lgwarlick.zenplannerplugin.Repository;
 
 import lgwarlick.zenplannerplugin.Model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ClientRepository extends CrudRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
