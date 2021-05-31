7cfbb59f7f6c520e552c6b052bf050ba4414e13c
Logan <lgwarlick@gmail.com>
2021-05-31 16:16:45 -0500

    added generated value annotation

--- src/main/java/lgwarlick/zenplannerplugin/Model/Client.java
+++ src/main/java/lgwarlick/zenplannerplugin/Model/Client.java
@@ -2,6 +2,8 @@ package lgwarlick.zenplannerplugin.Model;
 import com.opencsv.bean.CsvBindByName;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
@@ -18,6 +20,7 @@ public class Client {
     }
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @CsvBindByName
