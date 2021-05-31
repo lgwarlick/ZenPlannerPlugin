af4a3fee615298ed6fad2779adcabad209da5bb5
Logan <lgwarlick@gmail.com>
2021-05-31 11:36:11 -0500

    Added ClientRepository.java interface, extending CrudRepository

---
+++ src/main/java/lgwarlick/zenplannerplugin/Repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package lgwarlick.zenplannerplugin.Repository;
+
+import lgwarlick.zenplannerplugin.Model.Client;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientRepository extends CrudRepository<Client, Long> {
+}
