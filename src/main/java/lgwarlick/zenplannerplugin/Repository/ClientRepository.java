79416c58dbb52467f4929aff5f1b9250c3ed263e
Logan <lgwarlick@gmail.com>
2021-05-31 16:02:22 -0500

    added missing repository annotation

--- src/main/java/lgwarlick/zenplannerplugin/Repository/ClientRepository.java
+++ src/main/java/lgwarlick/zenplannerplugin/Repository/ClientRepository.java
@@ -3,6 +3,8 @@ package lgwarlick.zenplannerplugin.Repository;
 import lgwarlick.zenplannerplugin.Model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
