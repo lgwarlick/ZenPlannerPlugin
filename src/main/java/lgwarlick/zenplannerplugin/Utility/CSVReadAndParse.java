587cda28745e75aba79565a71d1d35dc32da88fd
Logan <lgwarlick@gmail.com>
2021-06-03 18:31:00 -0500

    first version of csv reader

---
+++ src/main/java/lgwarlick/zenplannerplugin/Utility/CSVReadAndParse.java
@@ -0,0 +1,42 @@
+package lgwarlick.zenplannerplugin.Utility;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import lgwarlick.zenplannerplugin.Model.Client;
+import lgwarlick.zenplannerplugin.Repository.ClientRepository;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class CSVReadAndParse {
+    private static final String CSV_PATH = "";
+
+    private ClientRepository clientRepository;
+
+    public CSVReadAndParse(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public void read() throws IOException {
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(CSV_PATH));
+                ){
+            CsvToBean<Client> csvToBean = new CsvToBeanBuilder<Client>(reader)
+                    .withType(Client.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            Iterator<Client> clientIterator = csvToBean.iterator();
+
+            Client unsavedClient = new Client();
+
+            while (clientIterator.hasNext()){
+                unsavedClient = clientIterator.next();
+                clientRepository.save(unsavedClient);
+            }
+        }
+    }
+}
